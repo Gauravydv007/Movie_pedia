b2941c882e8473dc5940ce66e5a57d057a6093b2
Gaurav Yadav <Gaurav88689548@gmail.com>
2025-09-04 19:58:35 +0530

    Navbar UI update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../css/Navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/Navbar.css";
 
 function Navbar() {
-  
   return (
-    <nav className='navbar'>
-        <div className='navbar-brand'>
-            <Link to="/">MoviePedia</Link>
-        </div>
-        <div className="navbar-links">
-            <Link to="/" className='nav-link' >Home</Link>
-            <Link to="/favorites" className='nav-link' >Favorites</Link>
-        </div>
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">MoviePedia</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/favorites" className="nav-link">
+          Favorites
+        </Link>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
