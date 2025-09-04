e7aa7fee9799a0fb38ea608ad39b0bc44b02d3d6
Gaurav Yadav <Gaurav88689548@gmail.com>
2025-09-04 19:56:00 +0530

    update Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import '../css/Navbar.css'
 
 function Navbar() {
+  
   return (
     <nav className='navbar'>
         <div className='navbar-brand'>
@@ -12,7 +13,6 @@ function Navbar() {
             <Link to="/" className='nav-link' >Home</Link>
             <Link to="/favorites" className='nav-link' >Favorites</Link>
         </div>
-        
     </nav>
   )
 }
