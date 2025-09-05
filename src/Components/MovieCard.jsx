793fcbc60c67e6a62d91dc5c5d8c4240d97235d8
Gaurav Yadav <Gaurav88689548@gmail.com>
2025-09-06 00:02:42 +0530

    Card Changes

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -19,6 +19,7 @@ const MovieCard = ({movie}) => {
         }
     }
   return (
+    
     <div className='movie-card'>
         <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
